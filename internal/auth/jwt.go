926899d2ad98efb0a924730f9fc57b6cbcd387f4
ekastn <118880703+ekastn@users.noreply.github.com>
2025-12-01 03:43:34 +0700

    feat: password and JWT helpers

---
+++ internal/auth/jwt.go
@@ -0,0 +1,45 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateAccessToken(userID, role, secret string) (string, error) {
+	claims := Claims{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(2 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
+
+func GenerateRefreshToken() (string, error) {
+	rnd, err := randomString(12)
+	if err != nil {
+		return "", err
+	}
+	return "rf_" + time.Now().Format("20060102_150405.000000") + "_" + rnd, nil
+}
+
+func randomString(n int) (string, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b)[:n], nil
+}
