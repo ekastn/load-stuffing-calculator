48b4b0ab48787c9ac14c2209d72d25ad037d25d4
ekastn <118880703+ekastn@users.noreply.github.com>
2026-02-03 01:17:49 +0700

    feat(mobile): basic profile page

--- mobile/lib/config/routes.dart
+++ mobile/lib/config/routes.dart
@@ -5,6 +5,7 @@ import '../pages/auth/login_page.dart';
 import '../pages/main_shell_page.dart';
 import '../pages/plans/plan_list_page.dart';
 import '../pages/plans/plan_form_page.dart';
+import '../pages/profile/profile_page.dart';
 import '../pages/resources/container_form_page.dart';
 import '../pages/resources/container_list_page.dart';
 import '../pages/resources/product_form_page.dart';
@@ -52,16 +53,7 @@ GoRouter createRouter(AuthProvider authProvider) {
           ),
           GoRoute(
             path: '/profile',
-            builder: (context, state) {
-              return Scaffold(
-                body: Center(
-                  child: ElevatedButton(
-                    onPressed: () => context.read<AuthProvider>().logout(),
-                    child: const Text('Logout'),
-                  ),
-                ),
-              );
-            },
+            builder: (context, state) => const ProfilePage(),
           ),
           GoRoute(
             path: '/products',
