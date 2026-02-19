de139b1c820528b111147a03d71fef7a829f44de
ekastn <118880703+ekastn@users.noreply.github.com>
2026-02-20 01:19:18 +0700

    feat: add SKU support for products

--- internal/dto/product_dto.go
+++ internal/dto/product_dto.go
@@ -2,6 +2,7 @@ package dto
 
 type CreateProductRequest struct {
 	Name     string  `json:"name" binding:"required,min=2,max=150"`
+	SKU      *string `json:"sku"`
 	LengthMM float64 `json:"length_mm" binding:"required,gt=0"`
 	WidthMM  float64 `json:"width_mm" binding:"required,gt=0"`
 	HeightMM float64 `json:"height_mm" binding:"required,gt=0"`
@@ -11,6 +12,7 @@ type CreateProductRequest struct {
 
 type UpdateProductRequest struct {
 	Name     string  `json:"name" binding:"required,min=2,max=150"`
+	SKU      *string `json:"sku"`
 	LengthMM float64 `json:"length_mm" binding:"required,gt=0"`
 	WidthMM  float64 `json:"width_mm" binding:"required,gt=0"`
 	HeightMM float64 `json:"height_mm" binding:"required,gt=0"`
@@ -21,6 +23,7 @@ type UpdateProductRequest struct {
 type ProductResponse struct {
 	ID       string  `json:"id"`
 	Name     string  `json:"name"`
+	SKU      *string `json:"sku,omitempty"`
 	LengthMM float64 `json:"length_mm"`
 	WidthMM  float64 `json:"width_mm"`
 	HeightMM float64 `json:"height_mm"`
